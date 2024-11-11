757b71d9221c919f8638f9f14176ef69a7916764
YeasinRiyad <mazumderyeasin98@gmail.com>
2024-11-12 00:07:39 +0600

    feat: complete TopMiddle component with responsive design and improved layout

---
+++ src/components/MiddleContainer.jsx
@@ -0,0 +1,17 @@
+import { twMerge } from "tailwind-merge";
+import PropTypes from 'prop-types';
+
+const MiddleContainer = ({ children, className }) => {
+    return (
+        <div className='w-full'>
+          <div className={twMerge("mx-auto py-12 w-full md:w-11/12 px-10 lg:w-10/12 lg:py-28 ", className)}>
+          {children}
+          </div>
+        </div>
+      );
+}
+MiddleContainer.propTypes = {
+    children: PropTypes.node.isRequired,
+    className: PropTypes.string,
+  };
+export default MiddleContainer
--- src/general/Main.jsx
+++ src/general/Main.jsx
@@ -1,11 +1,13 @@
 import Navbar from './header/Navbar'
 import Hero from './header/Hero'
+import TopMiddle from '../components/Navbar/MiddleSection/TopMiddle'
 
 const Main = () => {
   return (
    <div>
      <Navbar/>
      <Hero/>
+     <TopMiddle/>
    </div>
   )
 }
